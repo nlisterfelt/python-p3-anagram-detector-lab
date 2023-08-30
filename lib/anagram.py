c3c47f78724ca7ef25aa7da20f70d80e44782e1c
Nicole Listerfelt <nlisterfelt@gmail.com>
2023-08-30 17:25:52 -0500

    matching words

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,9 @@
-# your code goes here!
+class Anagram():
+    def __init__(self, word):
+        self.word = word
+    def match(self, words_list):
+        new_list = []
+        for new_word in words_list:
+            if set(new_word) == set(self.word):
+                new_list.append(new_word)
+        return new_list
